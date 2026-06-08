0bd9851d9b2dc744224cf895726ea59e29dde469
Michael Shatilo <mikhail.shatsila@innowise.com>
2026-06-08 10:55:50 +0200

    chore(backend): scaffold nx + nestjs workspace with foundation deps

---
+++ backend/apps/api/jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  displayName: 'api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/api',
+  testPathPattern: 'apps/api/src',
+  testMatch: ['**/__tests__/**/*.spec.ts', '**/*.spec.ts', '**/*.e2e-spec.ts'],
+};
